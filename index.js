b2769db24455471ee08da26635557cf3323e3912
Matthew Ung <matthew.ung814@gmail.com>
2017-09-30 12:05:31 -0700

    work in progress

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const Discord = require('discord.js');
+//const Discord = require('discord.js');
+const commando = require('discord.js-commando');
 const config = require('./config.js');
-const bot = new Discord.Client();
 
-bot.on('message', (message) => {
-    if(message.content == 'ping'){
+//const bot = new Discord.Client();
 
-        //message.channel.sendMessage('pong');
-        message.reply('pong');
-    }
-});
+//bot.on('message', (message) => {
+//    if(message.content == 'ping'){
+//
+//        //message.channel.sendMessage('pong');
+//        message.reply('pong');
+//    }
+//});
+
+const bot = new commando.Client();
+
+console.log("first");
+bot.registry.registerGroup('random', 'Random');
+console.log("second");
+bot.registry.registerDefaults();
+console.log("third");
+bot.registry.registerCommandsIn(__dirname + "/commands");
 
 bot.login(config.token);
